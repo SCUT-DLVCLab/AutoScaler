f4b5c72978046be99f0e83d6a682ce71e3711eee
Geong <38862569+W-Geong@users.noreply.github.com>
2025-06-08 16:27:35 +0800

    Add files via upload

--- AutoScaler/test.py
+++ AutoScaler/test.py
@@ -1,21 +1,19 @@
 from pytorch_lightning import Trainer
-from bttr.datamodule import UniDatamodule3
-from bttr.lit_bttr import LitBTTR
+from autos.datamodule import UniDatamodule3
+from autos.lit_autos import LitAutos
 import os
 
 
-ckp_path = "lightning_logs/version_0/checkpoints/epoch=11-step=360000-val_loss=0.2322.ckpt"
+ckp_path = "lightning_logs/version_0/checkpoints/epoch=199.ckpt"
 
 if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-    # datasets=['hme100k','mlhme38k','csdb_mini','zinc','table_bank','didi','didi_no_text','primus']
-    datasets=['hme100k']
+
+    datasets=['m2e']
     trainer = Trainer(logger=False, accelerator='gpu')
 
-    # dm = UniDatamodule3(datasets = datasets, batch_size = 1, num_workers = 64, scaled_heights=list(range(192,337,8)))
+    dm = UniDatamodule3(datasets = datasets, batch_size = 1, num_workers = 64, scaled_heights=list(range(192,337,8)))
 
-    dm = UniDatamodule3(datasets = datasets, batch_size = 1, num_workers = 0, scaled_heights=[80])
-
-    model = LitBTTR.load_from_checkpoint(ckp_path)
+    model = LitAutos.load_from_checkpoint(ckp_path)
 
     trainer.test(model, datamodule=dm)
